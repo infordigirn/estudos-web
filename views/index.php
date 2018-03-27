3d104f62fc5b14685f55535dadda4f9395b8b1e5
infordigirn <infordigidevelopment@gmail.com>
2018-03-27 13:21:18 -0300

    teste de merge

--- views/index.php
+++ views/index.php
@@ -8,5 +8,9 @@
 </head>
 <body>
 	<h1>teste git teste merge</h1>
+	<form action="index_submit" method="<" accept-charset="utf-8">
+		<label><input type="text/submit/hidden/button/etc" name="nome">Nome:</label>
+		<input type="text" name="nome">
+	</form>
 </body>
 </html>
