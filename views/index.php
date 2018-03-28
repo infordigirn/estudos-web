58dc77a1e58b90e397c93cae16c5810bb83600b3
infordigirn <infordigidevelopment@gmail.com>
2018-03-28 10:30:54 -0300

    forcando conflito de merge na master

--- views/index.php
+++ views/index.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	<h1>teste git teste merge</h1>
+	<h1>git teste merge</h1>
 	<form action="index_submit" method="<" accept-charset="utf-8">
 		<label><input type="text/submit/hidden/button/etc" name="nome">Nome:</label>
 		<input type="text" name="nome">
@@ -15,9 +15,9 @@
 	<div class="teste">
 		<nav>
 			<ul>
-				<li>Git</li>
+				<li>GitHub</li>
 				<li>Commit</li>
-				<li>Merge</li>
+				<li>MergeStable</li>
 			</ul>
 		</nav>
 	</div>
