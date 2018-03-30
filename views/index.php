b389ce593f3458a30ecf75197261ff8cdc71b13c
infordigirn <infordigidevelopment@gmail.com>
2018-03-30 13:12:35 -0300

    teste de push

--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Prestador</title>
+	<title>Prestador Teste</title>
 	<link rel="stylesheet" href="">
 </head>
 <body>
