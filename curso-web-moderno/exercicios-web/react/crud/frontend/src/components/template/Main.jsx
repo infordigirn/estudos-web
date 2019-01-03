5b7df554e13ca9ff8aa8b872a4dcbacc8a4d7542
infordigi <infordigirn@gmail.com>
2019-01-03 16:06:52 -0300

    crud em react

---
+++ curso-web-moderno/exercicios-web/react/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css'
+import React, { Fragment } from 'react'
+import Header from './Header'
+
+export default props =>
+    <Fragment>
+        <Header/>
+        <main className="content">
+            Conteúdo
+        </main>
+    </Fragment>
