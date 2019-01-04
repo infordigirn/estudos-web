4d7018d93e07c9af53f86583abe045f9395eec62
infordigi <infordigirn@gmail.com>
2019-01-04 19:49:51 -0300

    cadastro react

--- curso-web-moderno/exercicios-web/react/crud/frontend/src/components/user/UserCrud.jsx
+++ curso-web-moderno/exercicios-web/react/crud/frontend/src/components/user/UserCrud.jsx
@@ -139,7 +139,7 @@ export default class UserCrud extends Component {
                             onClick={() => this.load(user)}>
                             <i className="fa fa-pencil"></i>
                         </button>
-                        <button className="btn btn-danger ml-2"
+                        <button className="btn btn-danger"
                             onClick={() => this.remove(user)}>
                             <i className="fa fa-trash"></i>
                         </button>
